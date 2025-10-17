62b02ac08d987d34d8b2d0b2dc3e74536c048556
shcommit <happysh_s2@naver.com>
2025-10-17 15:23:45 +0900

    주식가격.js

--- 주식가격.js
+++ 주식가격.js
@@ -2,11 +2,6 @@
 const solution = (price) => {
     let answer = Array(price.length).fill(0)
     let tracked = price.reduce( (tracked, cur, idx) => {
-        // 다시 생각해보니 while문 조건이 있기 때문에 이로직 필요없음 ㅇㅅㅇ
-        // if (idx === 0) {
-        //     tracked.push([cur, 0])
-        //     return tracked
-        // }
         while (tracked.length > 0 && tracked.at(-1)[0] > cur) {
             let popped = tracked.at(-1)
             answer[popped[1]] = idx - popped[1]
