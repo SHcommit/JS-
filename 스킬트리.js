80df0b5a2d91a0ec0fc98eb44260e5635c4cda48
shcommit <happysh_s2@naver.com>
2025-10-19 01:59:09 +0900

    스킬트리.js

--- 스킬트리.js
+++ 스킬트리.js
@@ -1,7 +1,6 @@
 function solution(skill, skill_trees) {
-    let skillSet = new Set(skill)
     return skill_trees.reduce(((prev, _skill) => {
-        let filtered = [..._skill].filter (v => skillSet.has(v) ).join('') 
+        let filtered = [..._skill].filter (v => skill.includes(v) ).join('') 
         prev += skill.substring(0, filtered.length) === filtered ? 1 : 0
         return prev
     }), 0)
